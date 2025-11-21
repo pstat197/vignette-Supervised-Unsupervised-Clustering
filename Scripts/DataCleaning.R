20eb912645cc6d6064d5aae4601dcb8017f266ec
aartigaraye <aarti.garaye@gmail.com>
2025-11-20 18:30:09 -0800

    Data loading and cleaning, checking for missing values and duplicates

---
+++ Scripts/DataCleaning.R
@@ -0,0 +1,23 @@
+# Preprocessing data
+# setwd("/Users/aarti/Downloads/Capstone/197a/Vignette/vignette-UnsupervisedDBScan")
+
+library(readr)
+library(readxl)
+library(visdat)
+library(dplyr)
+library(caret)
+
+personality <- read.csv("data/raw/personality_synthetic_dataset.csv")
+head(personality_raw)
+
+# Which variables have missing data, if any
+vis_miss(personality) # No missing data yay! 
+
+# Converting the response variable as factoring
+personality$personality_type <- as.factor(personality$personality_type)
+
+# Summary Statistics
+summary(personality) 
+
+# Checking for Duplicates
+sum(duplicated(personality)) # No duplicates! 
